fb99838e058bbef33b948c6d583fe037f7299919
itlux <itluxa@gmail.com>
2023-01-20 15:20:08 +0500

    forma

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+const textarea = document.getElementById("message");
+textarea.addEventListener("keyup", (e) => {
+  textarea.style.height = "auto";
+  let scHeight = e.target.scrollHeight;
+  textarea.style.height = `${scHeight}px `;
+});
+
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviseId = "service_ey7xz1l";
+  const templateId = "template_rlk6i0c";
+
+  emailjs
+    .send(serviseId, templateId, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Habaringiz muvofaqiyatli jo'natildi");
+    })
+    .catch((err) => console.log(err));
+}
+
 var typingElement = document.querySelector(".typing-text");
 var typeArray = ["Programmer ", "SMM Manager", "Mobilographer"];
 var index = 0,
@@ -88,12 +117,6 @@ function playAnim() {
 
 // Start typing text
 playAnim();
-
-
-
-
-
-
 
 window.addEventListener("DOMContentLoaded", () => {
   const clock = new ProgressClock("#clock");
